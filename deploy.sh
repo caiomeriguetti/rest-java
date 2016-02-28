7c57bf9641bea9017cb2a20d9cb7c317cc213d8d
Caio Meriguetti <caiomeriguetti@gmail.com>
2016-02-27 22:21:46 -0300

    added some output messages to deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -13,20 +13,28 @@ appPath=$(appdir)
 tomcatAppsDir="/var/lib/tomcat7"
 
 #backend
+echo "================ BUILDING BACKEND ==============="
 cd $appPath/backend
 sudo mvn clean package
+echo "================ DEPLOYING BACKEND INTO TOMCAT ==============="
 sudo mkdir -p $tomcatAppsDir/servers-backend/
 sudo rm -rf $tomcatAppsDir/servers-backend/ROOT.war
 sudo rm -rf $tomcatAppsDir/servers-backend/ROOT
 sudo cp -a target/rest-java.war $tomcatAppsDir/servers-backend/ROOT.war
+echo "================ STARTING MONGODB FILES ==============="
+sudo mkdir $appPath/data
+sudo mongod --dbpath $appPath/data --port 27018
 
 #frontend
+echo "================ BUILDING FRONTEND ==============="
 cd $appPath/ui/src/main/webapp
 sudo npm install
 cd $appPath/ui
 sudo mvn clean package
+echo "================ DEPLOYING FRONTEND INTO TOMCAT ==============="
 sudo mkdir -p $tomcatAppsDir/servers-ui/
 sudo rm -rf $tomcatAppsDir/servers-ui/ROOT.war
 sudo rm -rf $tomcatAppsDir/servers-ui/ROOT
 sudo cp -a target/servers-ui.war $tomcatAppsDir/servers-ui/ROOT.war
+echo "================ RELOADING TOMCAT ==============="
 sudo service tomcat7 force-reload
