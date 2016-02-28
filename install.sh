fa61b5db72515a0b021672ac5a47413b5d7f3a1c
Caio Meriguetti <caiomeriguetti@gmail.com>
2016-02-27 22:18:05 -0300

    improved and fixed some bugs on install script

--- install.sh
+++ install.sh
@@ -5,16 +5,18 @@
 #Release:	14.04
 #Codename:	trusty
 
-function appdir {
-  pwd
-}
-
-appPath=$(appdir)
+appPath=$(pwd)
 
 tomcatConfigPath="/etc/tomcat7"
 
+sudo apt-get install -y software-properties-common python-software-properties
+sudo add-apt-repository ppa:webupd8team/java
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo apt-get update
+
 #jdk
-sudo apt-get install -y default-jdk
+sudo apt-get install -y oracle-java8-installer
+sudo apt-get install -y oracle-java8-set-default
 
 #tomcat7
 sudo apt-get install -y tomcat7
@@ -25,10 +27,17 @@ fi
 sudo cp $appPath/server.xml.template $tomcatConfigPath/server.xml
 
 #mongodb
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
 echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.2 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.2.list
-sudo apt-get update
 sudo apt-get install -y mongodb-org
 
 #maven
 sudo apt-get -y install maven
+
+#npm+node
+sudo apt-get -y install npm
+sudo apt-get -y install nodejs
+
+sudo apt-key update
+sudo apt-get update
+
+
